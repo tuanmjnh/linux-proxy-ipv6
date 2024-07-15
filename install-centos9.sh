f444e49ec368b843b4041348ba3ccdbfae9a948b
tuanmjnh <minhtuan200990@gmail.com>
2024-07-15 15:46:57 +0700

    2024

--- install-centos9.sh
+++ install-centos9.sh
@@ -13,12 +13,12 @@ gen64() {
 }
 install_3proxy() {
   echo "installing 3proxy"
-  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  URL="https://raw.githubusercontent.com/tuanmjnh/linux-proxy-ipv6/main/3proxy-0.9.4.tar.gz"
   wget -qO- $URL | bsdtar -xvf-
-  cd 3proxy-3proxy-0.8.6
+  cd 3proxy-0.9.4
   make -f Makefile.Linux
   mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-  cp src/3proxy /usr/local/etc/3proxy/bin/
+  cp src /usr/local/etc/3proxy/bin/
   cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
   chmod +x /etc/init.d/3proxy
   chkconfig 3proxy on
